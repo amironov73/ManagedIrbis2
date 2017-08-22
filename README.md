443d8069cf540066ef8714f8fec62e10de922b1b
Alexey Mironov <amironov73@gmail.com>
2017-08-22 13:46:13 +0800

    Initial project state

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ManagedIrbis2
-ManagedClient adapted for .NET Standard 2.0
+### ManagedIrbis
+ManagedIrbis2 is a framework for client development for
+popular russian library computer system IRBIS64 ("»–¡»—64").
+
+ManagedIrbis2 adapted for .NET Standard 2.0
---
+++ Source/Libs/AM.Core/AM/EmptyArray.cs
@@ -0,0 +1,37 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* EmptyArray.cs -- empty array shared instance to reduce memory traffic
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM
+{
+    /// <summary>
+    /// Empty array shared instance to reduce memory traffic.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public static class EmptyArray<T>
+    {
+        #region Fields
+
+        /// <summary>
+        /// Shared instance.
+        /// </summary>
+        [NotNull]
+        public static readonly T[] Value = new T[0];
+
+        #endregion
+    }
+}
