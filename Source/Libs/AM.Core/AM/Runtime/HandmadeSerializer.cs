eba447464f0256205c0cf804866801ed2d8b1a75
Alexey Mironov <amironov73@gmail.com>
2018-03-03 18:13:56 +0800

    Add classes: HandmadeSerializer, PrefixLength and Singleton

---
+++ Source/Libs/AM.Core/AM/Runtime/HandmadeSerializer.cs
@@ -0,0 +1,245 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* HandmadeSerializer.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.IO;
+using System.Reflection;
+using AM.IO;
+using AM.Logging;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM.Runtime
+{
+    /// <summary>
+    /// Simple serializer.
+    /// </summary>
+    [PublicAPI]
+    public sealed class HandmadeSerializer
+    {
+        #region Properties
+
+        /// <summary>
+        /// Namespace for short type names.
+        /// </summary>
+        [CanBeNull]
+        public string Namespace { get; set; }
+
+        /// <summary>
+        /// Assembly for short type names.
+        /// </summary>
+        [CanBeNull]
+        public Assembly Assembly { get; set; }
+
+        /// <summary>
+        /// Prefix length.
+        /// </summary>
+        public PrefixLength PrefixLength { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public HandmadeSerializer()
+        {
+            PrefixLength = PrefixLength.Full;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public HandmadeSerializer
+            (
+                PrefixLength prefixLength
+            )
+        {
+            PrefixLength = prefixLength;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Deserialize object.
+        /// </summary>
+        [NotNull]
+        public IHandmadeSerializable Deserialize
+            (
+                [NotNull] BinaryReader reader
+            )
+        {
+            Sure.NotNull(reader, nameof(reader));
+
+            string typeName = reader.ReadString();
+
+            if (PrefixLength == PrefixLength.Short)
+            {
+                typeName = Namespace + "." + typeName;
+            }
+
+            Type type = ReferenceEquals(Assembly, null)
+                ? Type.GetType(typeName, true)
+                : Assembly.GetType(typeName, true);
+            IHandmadeSerializable result = (IHandmadeSerializable) Activator.CreateInstance(type);
+
+            result.RestoreFromStream(reader);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Deserialize array of objects.
+        /// </summary>
+        [NotNull]
+        [ItemNotNull]
+        public IHandmadeSerializable[] DeserializeArray
+            (
+                [NotNull] BinaryReader reader
+            )
+        {
+            Sure.NotNull(reader, nameof(reader));
+
+            int count = reader.ReadPackedInt32();
+            string typeName = reader.ReadString();
+
+            if (PrefixLength == PrefixLength.Short)
+            {
+                typeName = Namespace + "." + typeName;
+            }
+
+            Type type = ReferenceEquals(Assembly, null)
+                ? Type.GetType(typeName, true)
+                : Assembly.GetType(typeName, true);
+
+            IHandmadeSerializable[] result = new IHandmadeSerializable[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                IHandmadeSerializable obj = (IHandmadeSerializable) Activator.CreateInstance(type);
+                obj.RestoreFromStream(reader);
+                result[i] = obj;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get name of specified type.
+        /// </summary>
+        [NotNull]
+        public string GetTypeName
+            (
+                [NotNull] object obj
+            )
+        {
+            Sure.NotNull(obj, nameof(obj));
+
+            Type type = obj.GetType();
+            string result;
+
+            switch (PrefixLength)
+            {
+                case PrefixLength.Short:
+                    result = type.Name;
+                    break;
+
+                case PrefixLength.Moderate:
+                    result = type.FullName;
+                    break;
+
+                case PrefixLength.Full:
+                    result = type.AssemblyQualifiedName;
+                    break;
+
+                default:
+                    Log.Error
+                        (
+                            nameof(HandmadeSerializer) + "::" + nameof(GetTypeName)
+                            + ": unexpected PrefixLength="
+                            + PrefixLength
+                        );
+
+                    throw new InvalidOperationException();
+            }
+
+            return result.ThrowIfNull("result");
+        }
+
+        /// <summary>
+        /// Serialize the object.
+        /// </summary>
+        [NotNull]
+        public HandmadeSerializer Serialize
+            (
+                [NotNull] BinaryWriter writer,
+                [NotNull] IHandmadeSerializable obj
+            )
+        {
+            Sure.NotNull(writer, nameof(writer));
+            Sure.NotNull(obj, nameof(obj));
+
+            string typeName = GetTypeName(obj);
+            writer.Write(typeName);
+
+            obj.SaveToStream(writer);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Serialize the object.
+        /// </summary>
+        [NotNull]
+        public HandmadeSerializer Serialize
+            (
+                [NotNull] BinaryWriter writer,
+                [NotNull][ItemNotNull] IHandmadeSerializable[] array
+            )
+        {
+            Sure.NotNull(writer, nameof(writer));
+            Sure.NotNull(array, nameof(array));
+
+            int count = array.Length;
+            if (count == 0)
+            {
+                Log.Error
+                    (
+                        nameof(HandmadeSerializer) + "::" + nameof(Serialize)
+                        + ": count=0"
+                    );
+
+                throw new ArgumentException();
+            }
+
+            writer.WritePackedInt32(count);
+
+            IHandmadeSerializable first = array[0];
+
+            string typeName = GetTypeName(first);
+            writer.Write(typeName);
+
+            foreach (IHandmadeSerializable obj in array)
+            {
+                obj.SaveToStream(writer);
+            }
+
+            return this;
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Libs/AM.Core/AM/Runtime/Singleton.cs
@@ -0,0 +1,112 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* Singleton.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM.Runtime
+{
+    /// <summary>
+    /// Хранилище объектов по принципу "каждого типа по одной штуке".
+    /// </summary>
+    [PublicAPI]
+    public static class Singleton
+    {
+        #region Private members
+
+        private static Dictionary<Type, object> _dictionary
+            = new Dictionary<Type, object>();
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Clear the dictionary.
+        /// </summary>
+        public static void Clear()
+        {
+            lock (_dictionary)
+            {
+                _dictionary.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Do we have instance of specified type?
+        /// </summary>
+        [Pure]
+        public static bool HaveInstance<T>()
+            where T : class
+        {
+            Type type = typeof(T);
+
+            lock (_dictionary)
+            {
+                return _dictionary.ContainsKey(type);
+            }
+        }
+
+        /// <summary>
+        /// Get the instance of the specified object type.
+        /// </summary>
+        public static T Instance<T>()
+                where T : class
+        {
+            T result;
+            Type type = typeof(T);
+
+            lock (_dictionary)
+            {
+                if (_dictionary.TryGetValue(type, out object value))
+                {
+                    result = (T)value;
+                }
+                else
+                {
+                    result = Activator.CreateInstance<T>();
+                    _dictionary[type] = result;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Remove the instance of the specified object type, if any.
+        /// </summary>
+        public static bool RemoveInstance<T>()
+            where T : class
+        {
+            Type type = typeof(T);
+
+            lock (_dictionary)
+            {
+                if (_dictionary.TryGetValue(type, out object value))
+                {
+                    if (value is IDisposable disposable)
+                    {
+                        disposable.Dispose();
+                    }
+                }
+
+                return _dictionary.Remove(type);
+            }
+        }
+
+        #endregion
+    }
+}
+
