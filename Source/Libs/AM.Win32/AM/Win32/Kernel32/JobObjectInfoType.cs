01a5cfd0f7dab3928a8dba426b94d6365540ed9f
Alexey Mironov <amironov73@gmail.com>
2018-03-09 13:02:19 +0800

    Code cleanup

--- Source/Libs/AM.Win32/AM/Win32/Kernel32/JobObjectInfoType.cs
+++ Source/Libs/AM.Win32/AM/Win32/Kernel32/JobObjectInfoType.cs
@@ -1,26 +1,13 @@
 ﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
-/* JobObjectInfoType.cs -- 
+/* JobObjectInfoType.cs -- limits for JobObject
  * Ars Magna project, http://arsmagna.ru
  * -------------------------------------------------------
  * Status: poor
  */
 
 #region Using directives
-
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using AM;
-using AM.Collections;
-using AM.IO;
-using AM.Runtime;
 
 using JetBrains.Annotations;
 
@@ -33,6 +20,7 @@ namespace AM.Win32
     /// for <see cref="Kernel32.SetInformationJobObject"/>.
     /// This parameter can be one of the following values.
     /// </summary>
+    [PublicAPI]
     public enum JobObjectInfoType
     {
         /// <summary>
