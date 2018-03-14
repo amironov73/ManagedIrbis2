b303ee556deeb1465780d9b497baf4952fbc1e77
Alexey Mironov <amironov73@gmail.com>
2018-03-14 18:02:58 +0800

    Add tests

---
+++ Source/UnitTests/AM/Collections/XRangeTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM.Collections;
+
+namespace UnitTests.AM.Collections
+{
+    [TestClass]
+    public class XRangeTest
+    {
+        [TestMethod]
+        public void XRange_Construction_1()
+        {
+            const int Length = 10;
+            XRange range = new XRange(Length);
+            Assert.AreEqual(0, range.Start);
+            Assert.AreEqual(Length, range.Length);
+        }
+
+        [TestMethod]
+        public void XRange_Construction_2()
+        {
+            const int Start = 1;
+            const int Length = 10;
+            XRange range = new XRange(Start, Length);
+            Assert.AreEqual(Start, range.Start);
+            Assert.AreEqual(Length, range.Length);
+        }
+
+        [TestMethod]
+        public void XRange_Enumeration_1()
+        {
+            const int Length = 10;
+            XRange range = new XRange(Length);
+
+            int[] array = range.ToArray();
+            
+            Assert.AreEqual(Length, array.Length);
+            for (int i = 0; i < Length; i++)
+            {
+                Assert.AreEqual(i, array[i]);
+            }
+        }
+
+        [TestMethod]
+        public void XRange_Enumeration_2()
+        {
+            const int Length = 10;
+            XRange range = new XRange(Length);
+
+            IEnumerator enumerator = range.GetEnumerator();
+            Assert.IsNotNull(enumerator);
+            for (int i = 0; i < Length; i++)
+            {
+                Assert.IsTrue(enumerator.MoveNext());
+                int item = (int) enumerator.Current;
+                Assert.AreEqual(i, item);
+            }
+            Assert.IsFalse(enumerator.MoveNext());
+        }
+    }
+}
---
+++ Source/UnitTests/AM/ExceptionEventArgsTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM;
+
+namespace UnitTests.AM
+{
+    [TestClass]
+    public class ExceptionEventArgsTest
+    {
+        [TestMethod]
+        public void ExceptionEventArgs_Construction_1()
+        {
+            Exception exception = new Exception("Hello");
+            ExceptionEventArgs<Exception> args
+                = new ExceptionEventArgs<Exception>(exception);
+            Assert.AreSame(exception, args.Exception);
+            Assert.IsFalse(args.Handled);
+        }
+
+        [TestMethod]
+        public void ExceptionEventArgs_Construction_2()
+        {
+            Exception exception = new Exception("Hello");
+            ExceptionEventArgs args = new ExceptionEventArgs(exception);
+            Assert.AreSame(exception, args.Exception);
+        }
+
+        [TestMethod]
+        public void ExceptionEventArgs_Handled_1()
+        {
+            Exception exception = new Exception("Hello");
+            ExceptionEventArgs args = new ExceptionEventArgs(exception)
+                {
+                    Handled = true
+                };
+            Assert.IsTrue(args.Handled);
+        }
+
+        [TestMethod]
+        public void ExceptionEventArgs_Handled_2()
+        {
+            Exception exception = new Exception("Hello");
+            ExceptionEventArgs<Exception> args
+                = new ExceptionEventArgs<Exception>(exception)
+                {
+                    Handled = true
+                };
+            Assert.IsTrue(args.Handled);
+        }
+    }
+}
