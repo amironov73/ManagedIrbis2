5eef71245f36bc1156119dcc7dcb128118f51561
Alexey Mironov <amironov73@gmail.com>
2018-04-03 08:20:25 +0800

    Add FieldMapper class with tests

--- Source/Libs/ManagedIrbis/Source/Mapping/SubFieldMapper.cs
+++ Source/Libs/ManagedIrbis/Source/Mapping/SubFieldMapper.cs
@@ -1,7 +1,7 @@
 ﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
-/* SubFieldAttribute.cs -- отображение подполя на свойство
+/* SubFieldMapper.cs -- отображение подполя на свойство
  * Ars Magna project, http://arsmagna.ru
  * -------------------------------------------------------
  * Status: poor
