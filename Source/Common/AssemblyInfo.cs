8735e4ca1b66927e0fc1acfb89ac70a613f3f6fc
Alexey Mironov <amironov73@gmail.com>
2019-01-26 13:50:36 +0800

    Update NuGet package references

--- Source/Common/AssemblyInfo.cs
+++ Source/Common/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyCompany("Ars Magna Project")]
-[assembly: AssemblyCopyright("Copyright © Alexey Mironov, 2006-2018")]
+[assembly: AssemblyCopyright("Copyright © Alexey Mironov, 2006-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
